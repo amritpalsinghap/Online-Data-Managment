64385e2d35552fb82d451c24384621ae14c7060f
amritpalap <amritpal500.ap@gmail.com>
2019-10-23 19:42:54 -0400

    deleted old project

--- sql/dbo.TimeTable.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[TimeTable] (
-    [CourseID]    INT        NOT NULL,
-    [ProfessorID] INT        NOT NULL,
-    [RoomNumber]  NCHAR (10) NOT NULL,
-    [StudentID] INT NOT NULL, 
-    FOREIGN KEY ([CourseID]) REFERENCES [dbo].[Course] ([CourseID]),
-    FOREIGN KEY ([ProfessorID]) REFERENCES [dbo].[Professor] ([ProfessorID])
-);
-
