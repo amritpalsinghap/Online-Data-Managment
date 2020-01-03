1c86e44b3e30004af179eaa41085487c6d33e659
amritpalap <amritpal500.ap@gmail.com>
2020-01-02 22:01:24 -0500

    install Bootsrap-vue and fixed an issue related to the vue-loader
    install bootstrap, bootstrap-vue, node-sass, vue-template-complier,

--- frontend/frontend/src/main.js
+++ frontend/frontend/src/main.js
@@ -3,9 +3,12 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import BootStrapVue from "bootstrap-vue";
+import "./custom.scss";
 
 Vue.config.productionTip = false;
-
+require('./components/global');
+Vue.use(BootStrapVue);
 new Vue({
   router,
   store,
