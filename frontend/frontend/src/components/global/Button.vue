1c71408ed1c6ea270be5186db9cbb193bd20afb8
amritpalap <amritpal500.ap@gmail.com>
2020-01-02 22:03:29 -0500

    Edited the button as global useable component

---
+++ frontend/frontend/src/components/global/Button.vue
@@ -0,0 +1,28 @@
+<template>
+    <b-button :variant="_color" @click="clickFunction"  >{{bText}}</b-button>
+</template>
+<script>
+export default {
+    name:'cstmButton',
+    props:
+    {
+        bText:
+        {
+            type:String,
+            required : true,
+        },
+        _color:
+        {
+            type:String,
+            required : true,
+            default:"secondary"
+        },
+        clickFunction:
+        {
+            type:Function,
+            required : false,
+        }
+    }
+    
+}
+</script>
