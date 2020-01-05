b992d64a79356386cfc959032aeaba2ff607c429
amritpalap <amritpal500.ap@gmail.com>
2020-01-04 22:30:39 -0500

    added containers for profile sections

--- frontend/frontend/src/views/Profile.vue
+++ frontend/frontend/src/views/Profile.vue
@@ -1,10 +1,17 @@
 <template>
-    <div>
-        
-    </div>
+  <div>
+    <b-container fluid id="profileSections">
+      <b-row>
+        <b-col id="section1">1 of 1 </b-col>
+        <b-col id="section2">
+          <b-row align-h="center" id="col2row1">1 of 2</b-row>
+          <b-row align-h="center" id="col2row2">2 of 2</b-row>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 <script>
-export default {
-    
-}
+export default {};
 </script>
+<style lang="scss"></style>
