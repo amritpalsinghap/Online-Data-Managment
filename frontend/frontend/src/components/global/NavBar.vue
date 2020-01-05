ff97fb540cc3872f1c667d7867fa755a05e5a8bb
amritpalap <amritpal500.ap@gmail.com>
2020-01-04 20:23:27 -0500

    created global component
    NavBar: still need some work to do
    list TODO: button link/name will change depend upon the user's emailaddress,

---
+++ frontend/frontend/src/components/global/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <b-navbar type="dark" variant="primary">
+      <b-navbar-brand to="/">
+        <img src="../../../public/img/icons/logo.jpg" alt="OnlineManagement" />
+      </b-navbar-brand>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item to="/">Home</b-nav-item>
+        <b-nav-item to="/about">About</b-nav-item>
+        <b-nav-item to="/login"  v-b-modal="modal-1">Login</b-nav-item>
+
+        <!-- Navbar dropdowns -->
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
+<script>
+export default {
+  name: "cstm-Navbar"
+};
+</script>
+<style scoped>
+img {
+  height: 30px;
+  width: 30px;
+}
+</style>
