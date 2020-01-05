6e85b52a9a67380743bf7c1700649229c03b9aee
amritpalap <amritpal500.ap@gmail.com>
2020-01-04 20:26:16 -0500

    created form for user credentials inputs
    will modif the component into signup/login/user Email page or modal(haven't decided yet)

---
+++ frontend/frontend/src/views/Login.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="login-card">
+    <b-card style="max-width: 20rem;" class="mb-2">
+      <label class="sr-only" for="inline-form-input-username">Username</label>
+      <b-form-input
+        id="inline-form-input-username"
+        placeholder="Username"
+        v-model="userName"
+      ></b-form-input>
+
+      <b-form-input
+        type="password"
+        id="text-password"
+        PLACEHOLDER="Password"
+        v-model="password"
+      ></b-form-input>
+
+      <cstm-button
+        _color="primary"
+        :clickFunction="checkCredentials"
+        bText="Login"
+      ></cstm-button>
+    </b-card>
+  </div>
+</template>
+<script>
+export default {
+  name: "login",
+
+  data() {
+    return {
+      userName: "",
+      password: ""
+    };
+  },
+  methods: {
+    checkCredentials() {
+      alert("Username: " + this.userName + "password: " + this.password);
+    }
+  }
+};
+</script>
+
+<style scoped>
+body {
+  padding: 1rem;
+}
+.login-card {
+  position: relative;
+}
+</style>
