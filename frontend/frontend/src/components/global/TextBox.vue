f6a576ae6b1e82462dc3eaab056d107f8df0f3ed
unknown <amritpal500.ap@gmail.com>
2020-02-05 11:29:10 -0500

    intialize Textbox component

---
+++ frontend/frontend/src/components/global/TextBox.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <b-form-group
+      label-cols="4"
+      label-cols-lg="2"
+      label-size="sm"
+      :label="labelText"
+      label-for="input-sm"
+    >
+      <b-form-input v-model="text" :placeholder="placeHolder"></b-form-input>
+    </b-form-group>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      text: ""
+    };
+  },
+  props: {
+    placeHolder: {
+      type: String,
+      require: true,
+      default: "Enter Here"
+    },
+    labelText: {
+      type: String,
+      require: true,
+      default: "TODO"
+    }
+  }
+};
+</script>
--- frontend/frontend/src/components/global/index.js
+++ frontend/frontend/src/components/global/index.js
@@ -2,3 +2,4 @@ import vue from "vue";
 
 vue.component("cstm-button", require("./Button.vue").default);
 vue.component("cstm-navbar", require("./NavBar.vue").default);
+vue.component("cstm-textbox", require("./TextBox.vue").default);
