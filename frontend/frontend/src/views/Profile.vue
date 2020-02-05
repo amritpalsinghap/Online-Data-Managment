05a5e6ff9bb6fabdfc42de52fc6be5e6567be587
unknown <amritpal500.ap@gmail.com>
2020-02-05 11:29:33 -0500

    added b-card

--- frontend/frontend/src/views/Profile.vue
+++ frontend/frontend/src/views/Profile.vue
@@ -1,17 +1,38 @@
 <template>
   <div>
-    <b-container fluid id="profileSections">
-      <b-row>
-        <b-col id="section1">1 of 1 </b-col>
-        <b-col id="section2">
-          <b-row align-h="center" id="col2row1">1 of 2</b-row>
-          <b-row align-h="center" id="col2row2">2 of 2</b-row>
-        </b-col>
-      </b-row>
-    </b-container>
+    <b-card-group class="profileSection rounded-lg">
+      <b-card id="box">
+        <b-card id="subbox1">1 of 1</b-card>
+      </b-card>
+      <b-card id="box2">
+        <b-card id="subbox2">1 of 2</b-card>
+        <b-card id="subbox3">2 of 2</b-card>
+      </b-card>
+    </b-card-group>
   </div>
 </template>
 <script>
 export default {};
 </script>
-<style lang="scss"></style>
+<style lang="scss">
+.profileSection {
+  padding: 20px 50px;
+}
+#box {
+  background-color: rgb(202, 214, 248);
+  padding: 20px 20px;
+}
+#box2 {
+  background-color: rgb(202, 214, 248);
+  padding: 20px 20px;
+}
+#subbox1 {
+  background-color: rgb(147, 176, 255);
+}
+#subbox2 {
+  background-color: rgb(147, 176, 255);
+}
+#subbox3 {
+  background-color: rgb(147, 176, 255);
+}
+</style>
